0fa818a3191aea899bd27c008ee4398e8bb4a00d
Francisco Maria Sousa Gonçalves Paiva <61988754+Francisco-Paiva@users.noreply.github.com>
2020-03-10 18:16:27 +0000

    Update main.c

--- main.c
+++ main.c
@@ -15,3 +15,54 @@ int main()
     interpretador(e);
     return  0;
 }
+
+/*#include <stdio.h>
+#include <math.h>
+
+void desenha_inicial(){
+
+	int let, num;
+
+	for (num = 1; num < 9; num++){
+
+		if (num = 1){
+			printf(" ");
+			for (let = 0; let < 7; let++){
+				printf (". ");
+			}
+			printf("2 ");
+			printf("\n");
+		}
+
+		if (num = 4) {
+			printf(" ");
+			for (let = 0; let < 4; let++){
+				printf (". ");
+			}
+			printf("* ");
+			for (let = 5; let < 8; let++){
+				printf (". ");
+			}
+			printf("\n");
+		}
+
+		if (num = 8){
+			printf(" ");
+			printf("1 ");
+			for (let = 0; let < 7; let++){
+				printf (". ");
+			}
+			printf("\n");
+		}
+
+		else {
+			for (let = 0; let < 8; let++){
+				printf (". ");
+			}
+		}
+	
+	}
+
+}
+
+*/
